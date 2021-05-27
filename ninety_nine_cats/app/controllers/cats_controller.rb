91cfe579354c715ac1d3e2ad19763997ced29261
Lori <erica.iida14@gmail.com>
2021-05-27 17:31:53 -0400

    started adding cats

--- ninety_nine_cats/app/controllers/cats_controller.rb
+++ ninety_nine_cats/app/controllers/cats_controller.rb
@@ -5,4 +5,29 @@ class CatsController < ApplicationController
     render :index
   end
 
+  def show
+    @cat = Cat.find(params[:id])
+    render :show
+  end 
+
+  def new
+    render :new
+  end 
+
+  def create 
+    @cat = Cat.new(cat_params)
+
+    if @cat.save 
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end 
+
+  end 
+
+  def cat_params
+    params.require(:cat).permit(:name, :color)
+
+  end 
+
 end
